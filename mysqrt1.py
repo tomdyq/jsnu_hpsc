291974ecf94e2a823b1e5ab75203f15c6296d87b
tomdyq <tomdyq@163.com>
2014-04-04 06:08:11 -0700

    add function

--- mysqrt1.py
+++ mysqrt1.py
@@ -1,11 +1,11 @@
-x = 4.
-s = 1.
-tol = 1.e-14
-for k in range(10000):
-    print "Before iteration %s, s = %s" % (k, s)
-    s0 = s
-    s = 0.5 * (s + x/s)
-    delta_s = s - s0
-    if abs(delta_s / x) < tol:
-        break
-print "After %s iterations, s = %s" % (k, s)
+def sqrt1(x):
+    s = 1.
+    tol = 1.e-14
+    for k in range(10000):
+        print "Before iteration %s, s = %s" % (k, s)
+        s0 = s
+        s = 0.5 * (s + x/s)
+        delta_s = s - s0
+        if abs(delta_s / x) < tol:
+            break
+    print "After %s iterations, s = %s" % (k, s)
